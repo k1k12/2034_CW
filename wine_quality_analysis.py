d4d7dfa6cbff09dd0ea7639d2f5e1b7c362b3a3f
k1k12 <sofiafrench@sky.com>
2025-04-20 17:53:29 +0100

    Plot graphs for red and white

--- wine_quality_analysis.py
+++ wine_quality_analysis.py
@@ -2,3 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+wine_red = pd.read_csv('datasets/winequality-red.csv')
+wine_white = pd.read_csv('datasets/winequality-white.csv')
+
+sns.countplot(data=wine_red, x='quality')
+sns.countplot(data=wine_white, x='quality')
