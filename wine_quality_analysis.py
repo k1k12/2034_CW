2dbb8c6f0b32e407e72266190ceddf0fc7d8ddf1
k1k12 <sofiafrench@sky.com>
2025-04-20 18:03:15 +0100

    Add sep, show models

--- wine_quality_analysis.py
+++ wine_quality_analysis.py
@@ -3,8 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-wine_red = pd.read_csv('datasets/winequality-red.csv')
-wine_white = pd.read_csv('datasets/winequality-white.csv')
+wine_red = pd.read_csv("datasets/winequality-red.csv",sep=";")
+wine_white = pd.read_csv("datasets/winequality-white.csv",sep=";")
+wine_red.columns = wine_red.columns.str.strip()
+wine_white.columns = wine_white.columns.str.strip()
 
-sns.countplot(data=wine_red, x='quality')
-sns.countplot(data=wine_white, x='quality')
+print(wine_red.head())
+print(wine_red.columns)
+
+sns.countplot(data=wine_red, x="quality")
+sns.countplot(data=wine_white, x="quality")
+
+plt.title("Wine Quality Distribution")
+plt.show()
